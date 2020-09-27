17d47cdf28beaf2814e3d6e2d1d541bad8ebfab7
PedroGaN <pgarcianavalon@gmail.com>
2020-09-27 17:19:42 +0100

    Calculator Protocol Stablished

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,7 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CalculatorProtocol {
+    
+    func buttonPressed(key: String) {
+        switch key {
+        case "Button0","Button1":
+            print(key)
+            
+        default:
+            <#code#>
+        }
+    }
+    
+    func add(num1: Float, num2: Float) -> Float {
+        
+        let result = num1 + num2
+        
+        return result;
+    }
+    
+    func subtraction(num1: Float, num2: Float) -> Float {
+        let result = num1 - num2
+        
+        return result;
+    }
+    
+    func multiply(num1: Float, num2: Float) -> Float {
+        let result = num1 * num2
+        
+        return result;
+    }
+    
+    func division(num1: Float, num2: Float) -> Float {
+        let result = num1 / num2
+        
+        return result;
+    }
+    
+    func module(num1: Int, num2: Int) -> Int {
+        let result = num1 % num2
+        
+        return result;
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,4 +59,14 @@ class ViewController: UIViewController {
 
 }
 
+protocol CalculatorProtocol {
+    
+    func add(num1: Float, num2: Float) -> Float
+    func subtraction(num1: Float, num2: Float) -> Float
+    func multiply(num1: Float, num2: Float) -> Float
+    func division(num1: Float, num2: Float) -> Float
+    func module(num1: Int, num2: Int) -> Int
+    func buttonPressed(key: String)
+}
+ 
 
